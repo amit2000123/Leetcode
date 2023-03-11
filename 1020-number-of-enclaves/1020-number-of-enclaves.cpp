8bd82b99da34d9aa1f58d24691cbaa8d0c07f8e2
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-03-11 12:05:47 +0530

    Time: 74 ms (81.52%), Space: 32.1 MB (37.89%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+
+    void dfs(int row, int col ,int n,int m,vector<vector<int>> &grid,vector<vector<int>> &vis){
+        vis[row][col] = 1;
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+                dfs(nrow,ncol,n,m,grid,vis);
+            }
+        }
+    }
+
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if( (i==0 || i==n-1 || j==0 || j==m-1) && grid[i][j] == 1  ){
+                    dfs(i,j,n,m,grid,vis);
+                }
+            }
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if( grid[i][j] == 1 && vis[i][j] == 0 ){
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+
+
+
+
+
+
+
+
+
+//  void bfs(int row, int col ,int n,int m,vector<vector<int>> &grid,     vector<vector<int>> &vis){
+//         queue<pair<int,int>>q;
+//         q.push({row,col});
+//         vis[row][col] = 1;
+//         int delrow[] = {-1,0,1,0};
+//         int delcol[] = {0,1,0,-1};
+//         while(q.size() != 0){
+//             int r = q.front().first;
+//             int c = q.front().second;
+//             q.pop();
+//             for(int i=0;i<4;i++){
+//             int nrow = r + delrow[i];
+//             int ncol = c + delcol[i];
+//             if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
+//                 vis[nrow][ncol] = 1;
+//                 q.push( {nrow,ncol} );
+//             }
+//         }
+//       }    
+//     }
+    
+//     int numEnclaves(vector<vector<int>> &grid) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<vector<int>> vis(n,vector<int>(m,0));
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if( (i==0 || i==n-1 || j==0 || j==m-1) && grid[i][j] == 1  ){
+//                     bfs(i,j,n,m,grid,vis);
+//                 }
+//             }
+//         }
+//         int c=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if( grid[i][j] == 1 && vis[i][j] == 0 ){
+//                     c++;
+//                 }
+//             }
+//         }
+//         return c;
+//     }
+};
