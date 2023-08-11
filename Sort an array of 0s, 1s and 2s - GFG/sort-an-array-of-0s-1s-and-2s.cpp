db45eefaf50fc4a3d2da4a31f1df0b4d726dfd85
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-08-11 15:39:05 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -8,19 +8,41 @@ using namespace std;
 class Solution
 {
     public:
+    // better Aprroach
+    // void sort012(int a[], int n)
+    // {
+    //     int c0 = 0 ,c1=0, c2=0;
+    //     for(int i=0;i<n;i++){
+    //         if(a[i] == 0) c0++;
+    //         else if(a[i] == 1) c1++;
+    //         else c2++;
+    //     }
+    //     for(int i=0;i<c0;i++) a[i] = 0;
+    //     for(int i=0;i<c1;i++) a[i+c0] = 1;
+    //     for(int i=0;i<c2;i++) a[i+c0+c1] = 2;
+        
+    // }
+    
+    // Best optimal Approach
+    
     void sort012(int a[], int n)
     {
-        int c0 = 0 ,c1=0, c2=0;
-        for(int i=0;i<n;i++){
-            if(a[i] == 0) c0++;
-            else if(a[i] == 1) c1++;
-            else c2++;
+        // 00000(low-1)   (low)11111(mid-1)  mid.......high   (high+1)22222(n-1)
+        int low = 0 , mid = 0 , high = n-1;
+        while(mid <= high){
+            if(a[mid] == 0){
+                swap( a[mid] , a[low] );
+                low++;
+                mid++;
+            }
+            else if(a[mid] == 1) mid++;
+            else{
+                swap(a[mid] , a[high]);
+                high--;
+            }
         }
-        for(int i=0;i<c0;i++) a[i] = 0;
-        for(int i=0;i<c1;i++) a[i+c0] = 1;
-        for(int i=0;i<c2;i++) a[i+c0+c1] = 2;
-        
     }
+    
     
 };
 
