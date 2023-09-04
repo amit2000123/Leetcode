ef34219ec353d9635896b3973559d3f9cbe2b3e5
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-09-04 21:05:41 +0530

    Added solution - LeetHub

---
+++ Search in a matrix - GFG/search-in-a-matrix.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int matSearch (vector <vector <int>> &arr, int n, int m, int target)
+	{
+	    for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr[0].size();j++){
+                if(arr[i][j] == target) return true;
+            }
+        }
+        return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--){
+        int n, m; cin >> n >> m;
+        vector <vector <int>> arr (n, vector <int> (m));
+        for (int i=0;i<n;i++)
+            for (int j=0;j<m;j++)
+                cin >> arr[i][j];
+                
+        int x; cin >> x;
+        Solution ob;
+        cout << ob.matSearch (arr, n, m, x) << endl;
+    }
+}
+// } Driver Code Ends
