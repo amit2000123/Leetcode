0b47944d62ec2ddbfae991cb1dab6c7ec3f90dd0
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-09-12 23:22:56 +0530

    Added solution - LeetHub

---
+++ Find the Highest number - GFG/find-the-highest-number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        int i;
+        for(i=0;i<a.size();){
+            if(a[i] < a[i+1]) i++;
+            else break;
+        }
+        return a[i];
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	vector<int>a(n);
+    	for(int i = 0; i < n; i++)
+    		cin>>a[i];
+    	Solution ob;
+    	int ans = ob.findPeakElement(a);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
