7cf8e0700c242feae63c824558db5cb9d98aad13
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-09-13 01:13:11 +0530

    Added solution - LeetHub

--- Majority Vote - GFG/majority-vote.cpp
+++ Majority Vote - GFG/majority-vote.cpp
@@ -31,20 +31,68 @@ class Solution {
     
     
     
+    // vector<int> Solve(int n, vector<int>& arr) {
+        
+    //     vector<int> ans;
+    //     unordered_map<int,int> m;
+        
+    //     for(int i=0;i<n;i++){
+    //         m[arr[i]]++;
+    //         if(m[arr[i]] == n/3 + 1) ans.push_back(arr[i]);
+    //     }
+    //     sort(ans.begin(),ans.end());
+    //     if(ans.size() == 0) return {-1};
+    //     return ans;
+    // }
+    
+    
+    
+    // Optimal
+    
+        
     vector<int> Solve(int n, vector<int>& arr) {
         
         vector<int> ans;
-        unordered_map<int,int> m;
-        
+        // int n = arr.size();
+        int cnt1 = 0 , cnt2 = 0;
+        int el1 , el2;
         for(int i=0;i<n;i++){
-            m[arr[i]]++;
-            if(m[arr[i]] == n/3 + 1) ans.push_back(arr[i]);
+            if(cnt1 == 0 && el2 != arr[i]){
+                cnt1++;
+                el1 = arr[i];
+            }
+
+            else if(cnt2 == 0 && el1 != arr[i]){
+                cnt2++;
+                el2 = arr[i];
+            }
+
+            else if(el1 == arr[i]){
+                cnt1++;
+            }
+
+            else if(el2 == arr[i]){
+                cnt2++;
+            }
+
+            else{
+                cnt1--;
+                cnt2--;
+            }
+
         }
-        sort(ans.begin(),ans.end());
+
+        int c1 = 0  , c2 = 0;
+        for(int i=0;i<n;i++){
+            if(el1 == arr[i]) c1++;
+            if(el2 == arr[i]) c2++;
+        }
+        if(c1 > n/3) ans.push_back(el1);
+        if(c2 > n/3)ans.push_back(el2);
+        sort(ans.begin() , ans.end());
         if(ans.size() == 0) return {-1};
         return ans;
     }
-    
     
     
     
