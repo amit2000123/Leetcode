a0dcb97fae56c24c7defb01b1feec87a05161e48
AMIT PRAJAPATI <85548650+amit2000123@users.noreply.github.com>
2023-09-17 10:54:12 +0530

    Added solution - LeetHub

--- Count Reverse Pairs - GFG/count-reverse-pairs.cpp
+++ Count Reverse Pairs - GFG/count-reverse-pairs.cpp
@@ -10,23 +10,78 @@ using namespace std;
 
 class Solution {
   public:
-    int cnt = 0;
+    // int cnt = 0;
     
-    void countPairs(vector<int> &arr , int low , int mid , int high){
+    // void countPairs(vector<int> &arr , int low , int mid , int high){
+    //     int right = mid + 1;
+    //     for(int i=low;i<=mid;i++){
+    //         while(right <= high && arr[i] > 2 * arr[right]) right++;
+    //         cnt += right -(mid + 1);
+    //     }
+    // }
+    
+    // void mergeSort(vector<int> &arr , int low , int high){
+    //     if(low >= high) return;
+    //     int mid = (low + high) /2;
+    //     mergeSort(arr,low,mid);
+    //     mergeSort(arr,mid+1,high);
+    //     countPairs(arr,low,mid,high);
+    //     merge(arr,low,mid,high);
+    // }
+    
+    // void merge(vector<int> &arr ,int low , int mid , int high){
+    //     vector<int> temp;
+    //     int left = low , right = mid + 1;
+    //     while(left <= mid && right <= high){
+    //         if(arr[left] <= arr[right]){
+    //             temp.push_back(arr[left]);
+    //             left++;
+    //         }
+    //         else{
+    //             temp.push_back(arr[right]);
+    //             right++;
+    //         }
+    //     }
+        
+    //     while(left <= mid) temp.push_back(arr[left++]);
+    //     while(right <= high) temp.push_back(arr[right++]);
+        
+    //     for(int i=low;i<=high;i++){
+    //         arr[i] = temp[i - low];
+    //     }
+    // }
+    
+    // int countRevPairs(int n, vector<int> arr) {
+    //     mergeSort(arr, 0 , n-1);
+    //     return cnt;
+    // }
+    
+    
+    
+    
+    
+    
+    // int cnt = 0;
+    
+    int countPairs(vector<int> &arr , int low , int mid , int high){
+        int cnt = 0;
         int right = mid + 1;
         for(int i=low;i<=mid;i++){
             while(right <= high && arr[i] > 2 * arr[right]) right++;
             cnt += right -(mid + 1);
         }
+        return cnt;
     }
     
-    void mergeSort(vector<int> &arr , int low , int high){
-        if(low >= high) return;
+    int mergeSort(vector<int> &arr , int low , int high){
+        int cnt = 0;
+        if(low >= high) return 0;
         int mid = (low + high) /2;
-        mergeSort(arr,low,mid);
-        mergeSort(arr,mid+1,high);
-        countPairs(arr,low,mid,high);
+        cnt += mergeSort(arr,low,mid);
+        cnt += mergeSort(arr,mid+1,high);
+        cnt += countPairs(arr,low,mid,high);
         merge(arr,low,mid,high);
+        return cnt;
     }
     
     void merge(vector<int> &arr ,int low , int mid , int high){
@@ -51,9 +106,16 @@ class Solution {
         }
     }
     
+    
+    
+    
+    
+    
+    
+    
+    
     int countRevPairs(int n, vector<int> arr) {
-        mergeSort(arr, 0 , n-1);
-        return cnt;
+        return mergeSort(arr, 0 , n-1);
     }
 };
 
